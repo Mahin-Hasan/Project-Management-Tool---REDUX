ba945ce0c754a37434a04e6a14da72012429bf9d
Mahin-Hasan <mahinmtrs@gmail.com>
2024-03-09 14:43:08 +0600

    update readme

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -22,16 +22,23 @@ const MyTasks = () => {
   const { tasks, userSpecificTasks } = useSelector((state) => state.tasksSlice);
   const { name: userName } = useSelector((state) => state.userSlice);// renaming name as userName
   const [isOpen, setIsOpen] = useState(false);
+  const [taskId, settaskId] = useState(0);
   const dispatch = useDispatch();
+
+  const handleModal = (id) => {
+    settaskId(id);
+    setIsOpen(!isOpen);
+  }
 
   // console.log(name);
   useEffect(() => {
     dispatch(userTasks(userName));
   }, [userName, tasks, dispatch]);//also added dependencey task cz in any change in occured in task than the useEffect must trigger
 
+
   return (
     <div>
-      <TaskDetailsModal isOpen={isOpen} setIsOpen={setIsOpen} />
+      <TaskDetailsModal isOpen={isOpen} setIsOpen={setIsOpen} id={taskId} />
       <h1 className="text-xl my-3">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
         {
@@ -43,7 +50,7 @@ const MyTasks = () => {
               <h1>{item.title}</h1>
               <div className="flex gap-3">
                 <button
-                  onClick={() => setIsOpen(!isOpen)}
+                  onClick={() => handleModal(item.id)}
                   className="grid place-content-center" title="Details">
                   <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
                 </button>
--- src/components/tasks/TaskDetailsModal.jsx
+++ src/components/tasks/TaskDetailsModal.jsx
@@ -1,7 +1,16 @@
+import { useSelector } from 'react-redux';
 import Modal from '../ui/Modal';
 
-const TaskDetailsModal = ({isOpen, setIsOpen}) => {
-    return <Modal isOpen={isOpen} setIsOpen={setIsOpen}></Modal>
+const TaskDetailsModal = ({ isOpen, setIsOpen, id }) => {
+    const { tasks } = useSelector((state) => state.tasksSlice);
+
+    const task = tasks.find((item) => item.id === id); //searching specific task so find operation
+
+
+
+    return <Modal isOpen={isOpen} setIsOpen={setIsOpen} title={task?.title}>
+        {task?.description}
+    </Modal>
 };
 
 export default TaskDetailsModal;
