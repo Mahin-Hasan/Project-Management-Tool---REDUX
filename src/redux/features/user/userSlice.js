d9b1cfbdf65797b66e1e9d00e4cc63a5cd64ad81
Mahin-Hasan <mahinmtrs@gmail.com>
2024-03-09 22:21:39 +0600

    added signup functionality with loader and managed user state with redux

--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import auth from '../../../utils/firebase.config';
 
 const initialState = {
@@ -12,12 +12,18 @@ const initialState = {
 
 export const createUser = createAsyncThunk(
   'userSlice/createUser',
-  async ({ email, password }) => {
+  async ({ email, password, name }) => {
     const data = await createUserWithEmailAndPassword(auth, email, password);
+    await updateProfile(auth.currentUser, {
+      displayName: name,
+    })
 
     console.log(data);
 
-    return;
+    return {
+      email: data.user.email,
+      name: data.user.displayName,
+    };
 
   }
 )
@@ -25,7 +31,15 @@ export const createUser = createAsyncThunk(
 const userSlice = createSlice({
   name: 'userSlice',
   initialState,
-  reducers: {},
+  reducers: {
+    setUser: (state, { payload }) => {
+      state.name = payload.name;
+      state.email = payload.email;
+    },
+    toggleLoading: (state, { payload }) => {
+      state.isLoading = payload;
+    }
+  },
   extraReducers: (builder) => {
     builder.addCase(createUser.pending, (state) => {
       state.isLoading = true;
@@ -41,14 +55,17 @@ const userSlice = createSlice({
         state.name = payload.name;
         state.error = '';
       })
-      .addCase(createUser.rejected, (state) => {
-        state.isLoading = true;
+      .addCase(createUser.rejected, (state, action) => {
+        state.isLoading = false;
         state.isError = true;
         state.email = '';
         state.name = '';
-        state.error = '';
+        state.error = action.error.message;
       });
   },
 });
+
+
+export const { setUser, toggleLoading } = userSlice.actions;
 
 export default userSlice.reducer;
